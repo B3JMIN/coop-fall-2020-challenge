fac4363d85553a4c03461932e2334694161eb1f7
Benjamin <41429182+Sherlock20000501@users.noreply.github.com>
2020-07-10 14:52:10 -0400

    solution

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,58 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self.time = 0
+        self.history = []
+        self.operand = []
 
     def add(self, num: int):
-        pass
+        self.value += num
+        self.history.append(num)
+        self.operand.append("1")
+        self.time += 1
 
     def subtract(self, num: int):
-        pass
+        self.value -= num
+        self.history.append(num)
+        self.operand.append("-1")
+        self.time += 1
 
     def undo(self):
-        pass
+        if self.operand[-1]== "1":
+            self.value -= self.history[-1]
+
+        else:
+            self.value += self.history[-1]
+        self.time -= 1
+        # self.history.pop(-1)
+        # self.operand.pop(-1)
 
     def redo(self):
-        pass
+        if len(self.history) > self.time :
+            if self.operand[-1] == "1":
+                self.value += self.history[-1]
+            else:
+                self.value -= self.history[-1]
+            self.time += 1
+        else:
+            print("no redo avaible")
 
     def bulk_undo(self, steps: int):
-        pass
+        step = steps
+        while step > 0:
+            pos = step * -1
+            if self.operand[pos] == "1":
+                self.value -= self.history[pos]
+            else:
+                self.value += self.history[pos]
+            self.time += 1
 
     def bulk_redo(self, steps: int):
-        pass
+        step = steps
+        while step > 0:
+            pos = step * -1
+            if self.operand[pos] == "1":
+                self.value += self.history[pos]
+            else:
+                self.value -= self.history[pos]
+            self.time += 1
